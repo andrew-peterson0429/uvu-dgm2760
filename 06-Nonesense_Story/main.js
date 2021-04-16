49a737c0b08477c09e5c4d429b2c0786ff59171a
Andrew Peterson <andrewpeterson1021@gmail.com>
2021-04-16 14:07:18 -0600

    Added a period

--- 06-Nonesense_Story/main.js
+++ 06-Nonesense_Story/main.js
@@ -20,7 +20,7 @@ function tellStory() {
 console.log(nounArray);
 
     let myStory = `Once upon a time, there were four ${adjectiveArray[0]} ${nounArray[0]}s named Flopsy, Mopsy, Gobsy and Peter. They lived with their mother underneath a 
-    ${nounArray[1]} near a ${adjectiveArray[1]} ${nounArray[2]} Now, my dears, said ${adjectiveArray[2]} Mrs. Rabbit, you may go over the ${nounArray[3]} or around the ${nounArray[4]},
+    ${nounArray[1]} near a ${adjectiveArray[1]}. ${nounArray[2]} Now, my dears, said ${adjectiveArray[2]} Mrs. Rabbit, you may go over the ${nounArray[3]} or around the ${nounArray[4]},
      but do not go into Mr. McGregor's ${nounArray[5]}, your father had an accident there and he was put in a ${nounArray[6]} by Mrs. McGregor. Now ${verbArray[0]} along, and don't 
     get into mischief. I am going ${verbArray[1]}ing.`;
 
